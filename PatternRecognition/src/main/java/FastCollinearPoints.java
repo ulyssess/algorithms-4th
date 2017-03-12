e4ea5a2fa2ed0626cfc0d8370d1499112055ab8b
ulyssess <aichaowang@gmail.com>
2017-03-12 22:04:14 +0800

    添加PatternRecognition作业程序

---
+++ PatternRecognition/src/main/java/FastCollinearPoints.java
@@ -0,0 +1,21 @@
+/**
+ * Created by wangaichao on 17/3/12.
+ */
+public class FastCollinearPoints {
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points)     {
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments()
+    {
+        return 0;
+    }
+
+    // the line segments
+    public LineSegment[] segments()
+    {
+        return null;
+    }
+}
